a515cdd0be5b7f40e476ab4a1dc03ec634607317
YutoArake <k021g1420@g.neec.ac.jp>
2023-02-22 13:35:38 +0900

    ステージ読み込み(マップチップ)

--- SourceFiles/objects/Stage.cpp
+++ SourceFiles/objects/Stage.cpp
@@ -1,27 +1,119 @@
 #include "Stage.h"
+#include <cassert>
+#include <fstream>
 
 void Stage::Initialize()
 {
 	model_ = Model::Create("cube");
-	worldTransform_.Initialize();
-	worldTransform_.translation = { 0,-5,0 };
-	worldTransform_.scale = { 20,1,20 };
+	for (int y = 0; y < STAGE_HEIGHT; y++)
+	{
+		for (int x = 0; x < STAGE_WIDTH; x++)
+		{
+			worldTransform_[y][x].Initialize();
+			worldTransform_[y][x].translation = { 0.0f, -5.0f, 0.0f };
+		}
+	}
+
+	LoadMap(0);
 }
 
 void Stage::Update()
 {
 
 	model_->TextureUpdate();
-	worldTransform_.Update();
+	for (int y = 0; y < STAGE_HEIGHT; y++)
+	{
+		for (int x = 0; x < STAGE_WIDTH; x++)
+		{
+			worldTransform_[y][x].Update();
+		}
+	}
 }
 
 void Stage::Draw()
 {
-	model_->Draw(worldTransform_);
+	for (int y = 0; y < STAGE_HEIGHT; y++)
+	{
+		for (int x = 0; x < STAGE_WIDTH; x++)
+		{
+			model_->Draw(worldTransform_[y][x]);
+		}
+	}
 }
 
 
 void Stage::LoadMap(UINT16 stage)
 {
-	
+	// バッファをクリア
+	stageCommands.str("");
+	// 状態をクリア
+	stageCommands.clear(std::stringstream::goodbit);
+
+	LoadStageData(stage);
+
+	LoadStageCommands();
+}
+
+void Stage::LoadStageData(UINT16 stage)
+{
+	// ファイル
+	std::ifstream file;
+	// パスを取得
+	std::string stageNum[7] = { "", "1", "2", "3", "4", "5", "6" };
+	const std::string stagefile = "stages/";
+	const std::string filename = "stage" + stageNum[stage] + ".txt";
+	const std::string directoryPath = "Resources/" + stagefile + "/" + filename;
+	// ファイルを開く
+	file.open(directoryPath);
+	assert(file.is_open());
+	// ファイルの内容を文字列ストリームにコピー
+	stageCommands << file.rdbuf();
+
+	// ファイルを閉じる
+	file.close();
+}
+
+void Stage::LoadStageCommands()
+{
+	// 1行分の文字列を入れる変数
+	std::string line;
+	// マップチップ用
+	int x = 0;
+	int y = 0;
+
+	// コマンド実行ループ
+	while (getline(stageCommands, line)) {
+		// 1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+		// 文字列
+		std::string word;
+		// ,区切りで行の先頭文字列を取得
+		getline(line_stream, word, ',');
+
+		// "//"から始まる行はコメント
+		if (word.find("//") == 0) {
+			// コメント行を読み飛ばす
+			continue;
+		}
+
+		while (x != STAGE_WIDTH)
+		{
+			// コマンド読み込み
+			if (word.find("0") == 0) {
+			}
+			else if (word.find("1") == 0) {
+				Vector3 trans{};
+				trans.x = -9.5f + (1.0f * x);
+				trans.y = -5.0f;
+				trans.z = 9.5f - (1.0f * y);
+
+				worldTransform_[y][x].translation = trans;
+			}
+			// 次の内容へ
+			getline(line_stream, word, ',');
+			x++;
+		}
+		y++;
+		x = 0;
+	}
 }
--- SourceFiles/objects/Stage.h
+++ SourceFiles/objects/Stage.h
@@ -1,21 +1,28 @@
 #pragma once
 #include <basetsd.h>
 #include <Model.h>
-
+#include <sstream>
 
 
 class Stage
 {
+public:
+	static const int STAGE_WIDTH = 20;
+	static const int STAGE_HEIGHT = 20;
+
 public:
 	void Initialize();
 	void Update();
 	void Draw();
 
 	void LoadMap(UINT16 stage);
+	void LoadStageData(UINT16 stage);
+	void LoadStageCommands();
 
 private:
 	std::unique_ptr<Model> model_;
-	WorldTransform worldTransform_;
-
+	WorldTransform worldTransform_[STAGE_HEIGHT][STAGE_WIDTH];
+	// ファイルコマンド
+	std::stringstream stageCommands;
 };
 
