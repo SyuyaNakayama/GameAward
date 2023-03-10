0417b94c996acb84ba4fd594e78836332947d893
YutoArake <k021g1420@g.neec.ac.jp>
2023-03-10 13:50:58 +0900

    プレイヤーの押し戻し処理を微調整&コメント追加

--- SourceFiles/objects/Player.cpp
+++ SourceFiles/objects/Player.cpp
@@ -4,6 +4,13 @@
 #include <imgui.h>
 #include <algorithm>
 
+// 移動制限用定数
+const Vector2 STAGE_SIZE =
+{
+	Stage::STAGE_WIDTH - 1.0f, // 1.0fはズレの修正
+	Stage::STAGE_HEIGHT - 1.0f
+};
+
 void Player::Initialize()
 {
 	model_ = Model::Create("cube");
@@ -18,6 +25,8 @@ void Player::Initialize()
 
 void Player::Move()
 {
+	// 前フレーム座標取得
+	prePos = worldTransform.translation;
 	// 移動
 	float speed = 0.5f;
 	Vector3 move;
@@ -26,13 +35,7 @@ void Player::Move()
 	move = Quaternion::RotateVector(move, Quaternion::MakeAxisAngle(Vector3::MakeYAxis(), eyeCamera.GetAngleTarget()));
 	worldTransform.translation += move;
 
-	// 移動制限
-	const Vector2 STAGE_SIZE =
-	{
-		Stage::STAGE_WIDTH - 1.0f, // 1.0fはズレの修正
-		Stage::STAGE_HEIGHT - 1.0f
-	};
-
+	// 下限上限設定
 	worldTransform.translation.x = std::clamp(worldTransform.translation.x, -STAGE_SIZE.x, STAGE_SIZE.x);
 	worldTransform.translation.z = std::clamp(worldTransform.translation.z, -STAGE_SIZE.y, STAGE_SIZE.y);
 }
@@ -65,22 +68,27 @@ void Player::ChangeLight()
 
 void Player::OnCollision(BoxCollider* boxCollider)
 {
-	// 移動
-	float speed = -0.5f;
-	Vector3 move;
-	move.z = input_->Move(Key::W, Key::S, speed);
-	move.x = input_->Move(Key::D, Key::A, speed);
-	move = Quaternion::RotateVector(move, Quaternion::MakeAxisAngle(Vector3::MakeYAxis(), eyeCamera.GetAngleTarget()));
-	worldTransform.translation += move;
-
-	// 移動制限
-	const Vector2 STAGE_SIZE =
-	{
-		Stage::STAGE_WIDTH - 1.0f, // 1.0fはズレの修正
-		Stage::STAGE_HEIGHT - 1.0f
-	};
-
-	worldTransform.translation.x = std::clamp(worldTransform.translation.x, -STAGE_SIZE.x, STAGE_SIZE.x);
-	worldTransform.translation.z = std::clamp(worldTransform.translation.z, -STAGE_SIZE.y, STAGE_SIZE.y);
+	// それぞれの座標、半径取得
+	Vector3 boxPos = boxCollider->GetWorldPosition();
+	Vector3 boxRadius = boxCollider->GetRadius();
+	Vector3 playerRadius = GetRadius();
+	// 前フレームとの差で侵入方向を確認する
+	if (prePos.x < boxPos.x - boxRadius.x) {
+		// ボックスよりも左側に押し出す
+		worldTransform.translation.x = std::clamp(worldTransform.translation.x, -STAGE_SIZE.x, boxPos	.x - boxRadius.x - playerRadius.x);
+	}
+	else if (prePos.x > boxPos.x  + boxRadius.x) {
+		// ボックスよりも左側に押し出す
+		worldTransform.translation.x = std::clamp(worldTransform.translation.x, boxPos.x + boxRadius.x + playerRadius.x, STAGE_SIZE.x);
+	}
+	else if (prePos.z < boxPos.z - boxRadius.z) {
+		// ボックスよりも下側に押し出す
+		worldTransform.translation.z = std::clamp(worldTransform.translation.z, -STAGE_SIZE.y, boxPos.z - boxRadius.z - playerRadius.z);
+	}
+	else if (prePos.z > boxPos.z + boxRadius.z) {
+		// ボックスよりも上側に押し出す
+		worldTransform.translation.z = std::clamp(worldTransform.translation.z, boxPos.z + boxRadius.z + playerRadius.z, STAGE_SIZE.y);
+	}
+	// 行列の更新
 	worldTransform.Update();
 }
--- SourceFiles/objects/Player.h
+++ SourceFiles/objects/Player.h
@@ -9,24 +9,34 @@ class Player : public BoxCollider
 {
 private:
 	Input* input_;
-	bool useLight;
 	std::unique_ptr<Model> model_;
 	std::unique_ptr<Sprite> sprite_;
+	// カメラ
 	Camera eyeCamera;
 	bool isCameraChange = false;
+	// ライト
 	LightGroup* lightGroup_;
+	bool useLight;
+	// 前フレーム座標
+	Vector3 prePos;
 
+	// 移動処理
 	void Move();
+	// ライト切り替え
 	void ChangeLight();
+
 public:
 	void Initialize();
 	void Update();
 	void Draw();
 
+	// アクセッサ
 	WorldTransform GetWorldTransform() { return worldTransform; }
 	bool IsCameraChange() { return isCameraChange; }
+	// デストラクタ
 	~Player() { sprite_.release(); }
 
+	// 当たり判定の処理
 	void OnCollision(BoxCollider* boxCollider) override;
 	//void OnCollision(SphereCollider* sphereCollider) override;
 	//void OnCollision(PlaneCollider* boxCollider) override;
