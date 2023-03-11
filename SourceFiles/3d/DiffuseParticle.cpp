7e2e817cb59131697c1b9cbbf83ed419072d8870
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2023-03-11 23:09:56 +0900

    DiffuseParticle完成(仮)

--- SourceFiles/3d/DiffuseParticle.cpp
+++ SourceFiles/3d/DiffuseParticle.cpp
@@ -1,1 +1,42 @@
 #include "DiffuseParticle.h"
+#include <random>
+
+void DiffuseParticle::Particle::Update()
+{
+	velocity += accel;
+	position += velocity;
+	float f = 1.0f / frame.GetRemainTimeRate();
+	scale = s_scale + (e_scale - s_scale) / f;
+}
+
+void DiffuseParticle::Add(const AddProp& particleProp)
+{
+	std::random_device rnd;
+	std::mt19937 mt(rnd());
+	std::uniform_real_distribution<float> randPos(-particleProp.posRange, particleProp.posRange);
+	std::uniform_real_distribution<float> randVel(-particleProp.velRange, particleProp.velRange);
+	std::uniform_real_distribution<float> randAcc(-particleProp.accRange, particleProp.accRange);
+
+	for (unsigned short i = 0; i < particleProp.addNum; i++)
+	{
+		//if (particles.size() >= (size_t)vertexCount / sizeof(VertexPos)) { return; }
+		particles.emplace_front();
+		Particle& p = particles.front();
+		p.position = Vector3(randPos(mt), randPos(mt), randPos(mt)) + particleProp.posOffset;
+		p.velocity = Vector3(randVel(mt), randVel(mt), randVel(mt)) + particleProp.velOffset;
+		p.accel = Vector3(randAcc(mt), randAcc(mt), 0) + particleProp.accOffset;
+		p.frame = particleProp.lifeTime;
+		p.scale = p.s_scale = particleProp.start_scale;
+		p.e_scale = particleProp.end_scale;
+	}
+}
+
+void DiffuseParticle::Update()
+{
+	particles.remove_if([](Particle& particle) { return particle.frame.CountDown(); });
+	for (auto& particle : particles) { particle.Update(); }
+}
+
+void DiffuseParticle::Draw()
+{
+}
--- SourceFiles/3d/DiffuseParticle.h
+++ SourceFiles/3d/DiffuseParticle.h
@@ -1,6 +1,45 @@
 #pragma once
+#include "Timer.h"
+#include <list>
+#include "Vector.h"
 
 // 拡散するパーティクル(今までのやつ)
 class DiffuseParticle
 {
+private:
+	// パーティクル1粒
+	struct Particle
+	{
+		Vector3 position{}; // 座標
+		Vector3 velocity{}; // 速度
+		Vector3 accel{}; // 加速度
+		Timer frame = 0;
+		float scale = 1.0f; // スケール
+		float s_scale = 1.0f; // 初期値
+		float e_scale = 0.0f;	// 最終値
+
+		void Update();
+	};
+
+	std::list<Particle> particles;
+
+public:
+	struct AddProp
+	{
+		Vector3 posOffset;
+		Vector3 velOffset;
+		Vector3 accOffset;
+		float posRange = 1.0f;
+		float velRange = 0.1f;
+		float accRange = 0.001f;
+		int lifeTime = 60;
+		float start_scale = 1.0f;
+		float end_scale = 0.0f;
+		unsigned short addNum = 1;
+	};
+
+	void Initialize() {  }
+	void Add(const AddProp& particleProp);
+	void Update();
+	void Draw();
 };
