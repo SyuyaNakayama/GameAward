1b87ff8ed1dbbd012f4f230692e8df1d90f413cc
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2023-05-16 16:35:31 +0900

    ステージセレクトのドアの上にステージテキスト(モデル)を付けた

--- SourceFiles/scene/TitleScene.h
+++ SourceFiles/scene/TitleScene.h
@@ -2,6 +2,17 @@
 #include "BaseScene.h"
 #include "Stage.h"
 
+class StagePlane
+{
+private:
+	WorldTransform worldTransform;
+	std::unique_ptr<Model> model;
+
+public:
+	void Initialize(const std::string& textureName, const Vector3& pos, const Vector3& scale);
+	void Draw() { model->Draw(worldTransform); }
+};
+
 class TitleScene : public BaseScene
 {
 private:
@@ -9,6 +20,8 @@ private:
 	LightGroup* lightGroup;
 	Stage stage;
 	Timer uiMoveTimer = 80;
+	// ステージプレーン
+	std::array<StagePlane, 5> stagePlanes;
 
 	void UI_Move(), UI_Camera();
 	void (TitleScene::* UIUpdate)() = nullptr;
