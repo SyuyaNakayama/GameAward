b0c0adc64a2f4c215b33dcd2d02c64aacd05a597
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-05-29 11:05:02 +0900

    とりあえず完成

--- SourceFiles/scene/ClearScene.cpp
+++ SourceFiles/scene/ClearScene.cpp
@@ -5,21 +5,39 @@ void ClearScene::Initialize()
 	viewProjection.Initialize();
 	debugCamera.Initialize();
 	Model::SetViewProjection(&debugCamera.GetViewProjection());
+	//部屋のモデル読み込み
+	models_object[0] = Model::Create("yuka");
+	models_object[1] = Model::Create("kabe");
+	models_object[2] = Model::Create("doorkabe");
+	models_object[3] = Model::Create("2door");
+	//プレイヤーのモデル読み込み
+	models_player[0] = Model::Create("player_body");	//頭
+	models_player[1] = Model::Create("player_shoesR");	//右足
+	models_player[2] = Model::Create("player_shoesL");	//左足
 
-	models[0] = Model::Create("yuka");
-	models[1] = Model::Create("kabe");
-	models[2] = Model::Create("doorkabe");
-	models[3] = Model::Create("2door");
-
+	for (int i = 0; i < 3; i++) {
+		playerTrans_[i].Initialize();
+	}
 	worldTransform_.Initialize();
+	worldTransform_.scale = { 3.0f,3.0f,3.0f };
+	//プレイヤーのポーズ調整
+	for (int i = 0; i < 3; i++) {
+		playerTrans_[i].rotation.y += 90 * PI / 180;
+		playerTrans_[i].translation += { 7.0f,-1.0f,0.0f };
+	}
+	playerTrans_[0].translation += {0.0f, 0.5f, 0.0f};
+	playerTrans_[1].rotation.x += 30 * PI / 180;
+	playerTrans_[2].rotation.x += -30 * PI / 180;
 }
 
 void ClearScene::Update()
 {
 	debugCamera.Update();
-	for(int i = 0;i < 4;i++)
-	{
-		models[i]->Update();
+	for(int i = 0;i < 4;i++){
+		models_object[i]->Update();
+	}
+	for (int i = 0; i < 3; i++) {
+		playerTrans_[i].Update();
 	}
 	worldTransform_.Update();
 }
@@ -28,9 +46,11 @@ void ClearScene::Draw()
 {
 	Model::PreDraw();
 	debugCamera.Update();
-	for (int i = 0; i < 4; i++)
-	{
-		models[i]->Draw(worldTransform_);
+	for (int i = 0; i < 4; i++){
+		models_object[i]->Draw(worldTransform_);
+	}
+	for (int i = 0; i < 3; i++) {
+		models_player[i]->Draw(playerTrans_[i]);
 	}
 	Model::PostDraw();
 }
--- SourceFiles/scene/ClearScene.h
+++ SourceFiles/scene/ClearScene.h
@@ -12,11 +12,9 @@ public:
 	void Draw();
 
 private:
-	std::unique_ptr<Model> models[4];
-	std::unique_ptr<Model> model_yaka;
-	std::unique_ptr<Model> model_kabe;
-	std::unique_ptr<Model> model_2door;
-	std::unique_ptr<Model> model_doorkabe;
+	std::unique_ptr<Model> models_object[4];
+	std::unique_ptr<Model> models_player[3];
 
 	WorldTransform worldTransform_;
+	WorldTransform playerTrans_[3];
 };
